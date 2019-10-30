2853029248087cc7eede45a687b5c5c906b17057
Florent Lioult <florent@hihan.org>
2019-10-30 09:35:17 +0100

    Adapted to changes in the core.

--- src/main/java/org/freedesktop/gstreamer/gl/GLContext.java
+++ src/main/java/org/freedesktop/gstreamer/gl/GLContext.java
@@ -11,48 +11,48 @@ import org.freedesktop.gstreamer.lowlevel.GstObjectPtr;
 // An OpenGL context, not a Gstreamer context.
 public class GLContext extends GstObject {
 
-	public static final String GTYPE_NAME = "GstGLContext";
+    public static final String GTYPE_NAME = "GstGLContext";
 
-	/**
-	 * Creates a new GLContext using the specified display.
-	 */
-	public GLContext(GLDisplay display) {
-		this(Natives.initializer(GSTGLCONTEXT_API.ptr_gst_gl_context_new(display)));
-	}
+    /**
+     * Creates a new GLContext using the specified display.
+     */
+    public GLContext(GLDisplay display) {
+        this(Natives.initializer(GSTGLCONTEXT_API.ptr_gst_gl_context_new(display)));
+    }
 
-	/**
-	 * This constructor is for internal use only.
-	 * 
-	 * @param init initialization data.
-	 */
-	protected GLContext(Initializer init) {
-		super(new Handle(init.ptr.as(GstObjectPtr.class, GstObjectPtr::new), init.ownsHandle), init.needRef);
-	}
+    /**
+     * This constructor is for internal use only.
+     * 
+     * @param init initialization data.
+     */
+    protected GLContext(Initializer init) {
+        super(new Handle(init.ptr.as(GstObjectPtr.class, GstObjectPtr::new), init.ownsHandle), init.needRef);
+    }
 
-	public void setToContext(Context context) {
-		// The static GLDisplay type is used, not the real instance type.
-		context.set("context", GTYPE_NAME, this);
-	}
+    public void setToContext(Context context) {
+        // The static GLDisplay type is used, not the real instance type.
+        context.getWritableStructure().setObject("context", GTYPE_NAME, this);
+    }
 
-	/**
-	 * Return the OpenGL context handle current to the calling thread
-	 * 
-	 * @param platform
-	 * @return the OpenGL context handle or NULL
-	 */
-	public static long getCurrentGLContext(final GLPlatform platform) {
-		return GSTGLCONTEXT_API.gst_gl_context_get_current_gl_context(platform);
-	}
+    /**
+     * Return the OpenGL context handle current to the calling thread
+     * 
+     * @param platform
+     * @return the OpenGL context handle or NULL
+     */
+    public static long getCurrentGLContext(final GLPlatform platform) {
+        return GSTGLCONTEXT_API.gst_gl_context_get_current_gl_context(platform);
+    }
 
-	private static final class Handle extends GstObject.Handle {
+    private static final class Handle extends GstObject.Handle {
 
-		public Handle(GstObjectPtr ptr, boolean ownsHandle) {
-			super(ptr, ownsHandle);
-		}
+        public Handle(GstObjectPtr ptr, boolean ownsHandle) {
+            super(ptr, ownsHandle);
+        }
 
-		@Override
-		protected void disposeNativeHandle(GPointer ptr) {
-			GSTGLCONTEXT_API.gst_gl_context_destroy(ptr.getPointer());
-		}
-	}
+        @Override
+        protected void disposeNativeHandle(GPointer ptr) {
+            GSTGLCONTEXT_API.gst_gl_context_destroy(ptr.getPointer());
+        }
+    }
 }
--- src/main/java/org/freedesktop/gstreamer/gl/GLDisplay.java
+++ src/main/java/org/freedesktop/gstreamer/gl/GLDisplay.java
@@ -7,36 +7,36 @@ import org.freedesktop.gstreamer.lowlevel.gl.GstGLDisplayAPI;
 
 public class GLDisplay extends GstObject {
 
-	public static final String GTYPE_NAME = "GstGLDisplay";
+    public static final String GTYPE_NAME = "GstGLDisplay";
 
-	/**
-	 * Creates a new GLDisplay using the system property GST_GL_PLATFORM.
-	 */
-	public GLDisplay() {
-		this(Natives.initializer(GstGLDisplayAPI.GSTGLDISPLAY_API.ptr_gst_gl_display_new()));
-	}
+    /**
+     * Creates a new GLDisplay using the system property GST_GL_PLATFORM.
+     */
+    public GLDisplay() {
+        this(Natives.initializer(GstGLDisplayAPI.GSTGLDISPLAY_API.ptr_gst_gl_display_new()));
+    }
 
-	/**
-	 * This constructor is for internal use only.
-	 * 
-	 * @param init initialization data.
-	 */
-	protected GLDisplay(Initializer init) {
-		super(init);
-	}
+    /**
+     * This constructor is for internal use only.
+     * 
+     * @param init initialization data.
+     */
+    protected GLDisplay(Initializer init) {
+        super(init);
+    }
 
-	public void setToContext(Context context) {
-		// The static GLDisplay type is used, not the real instance type.
-		context.set("gst.gl.GLDisplay", GTYPE_NAME, this);
-		// The native side also provide a dedicated setter doing the same thing:
-		// gst_context_set_gl_display'.
-	}
+    public void setToContext(Context context) {
+        // The static GLDisplay type is used, not the real instance type.
+        context.getWritableStructure().setObject("gst.gl.GLDisplay", GTYPE_NAME, this);
+        // The native side also provides a dedicated setter doing the same thing:
+        // gst_context_set_gl_display'.
+    }
 
-	public GLAPI getGLAPI() {
-		return GstGLDisplayAPI.GSTGLDISPLAY_API.gst_gl_display_get_gl_api(this);
-	}
+    public GLAPI getGLAPI() {
+        return GstGLDisplayAPI.GSTGLDISPLAY_API.gst_gl_display_get_gl_api(this);
+    }
 
-	public boolean addContext(GLContext context) {
-		return GstGLDisplayAPI.GSTGLDISPLAY_API.gst_gl_display_add_context(this, context);
-	}
+    public boolean addContext(GLContext context) {
+        return GstGLDisplayAPI.GSTGLDISPLAY_API.gst_gl_display_add_context(this, context);
+    }
 }
