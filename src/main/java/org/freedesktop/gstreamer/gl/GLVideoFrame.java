069ec0ce95dc69a23a013f930da80ce5ece8a1d9
Florent Lioult <florent@hihan.org>
2019-11-08 11:07:18 +0100

    Fixed the GLVideoFrame memory problem highlighted by the fix of ISSUE-171.

--- src/main/java/org/freedesktop/gstreamer/gl/GLVideoFrame.java
+++ src/main/java/org/freedesktop/gstreamer/gl/GLVideoFrame.java
@@ -41,11 +41,6 @@ public class GLVideoFrame {
         if (textures == null) {
             int flags = GstBufferAPI.GST_MAP_READ | GstGLMemoryAPI.GST_MAP_GL;
             GstVideoInfoPtr gstVideoInfoPtr = Natives.getPointer(info).as(GstVideoInfoPtr.class, GstVideoInfoPtr::new);
-
-            // Workaround refCount problem induces by "ISSUE-171 - fix ownership and ref if
-            // previously cached unowned".
-            Natives.ref(buffer);
-
             if (GstVideoFrameAPI.GSTVIDEOFRAME_API.gst_video_frame_map(struct, gstVideoInfoPtr, buffer, flags)) {
                 textures = new int[buffer.getMemoryCount()];
                 for (int i = 0; i < textures.length; ++i) {
@@ -59,7 +54,6 @@ public class GLVideoFrame {
         } else {
             throw new IllegalStateException("GL video frame is already mapped.");
         }
-
     }
 
     /**
@@ -68,6 +62,12 @@ public class GLVideoFrame {
     public void unmap() {
         if (textures != null) {
             GstVideoFrameAPI.GSTVIDEOFRAME_API.gst_video_frame_unmap(struct);
+            /*
+             * The 'unmap' will unref the buffer (if it did it in the 'map' in accordance to
+             * the GST_VIDEO_FRAME_MAP_FLAG_NO_REF flag) and this struct is not expected to
+             * keep a ref on it.
+             */
+            struct.buffer.disown();
             textures = null;
         } else {
             throw new IllegalStateException("GL video frame is not mapped.");
